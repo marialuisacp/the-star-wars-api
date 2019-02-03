1b457e14b6589a5b64282cd74d336de4743c9c9a
Maria Luisa <marialuisaufmg@gmail.com>
2019-02-03 19:21:50 -0200

    remove unnecessary property

--- src/components/starship/card/starshipCard.js
+++ src/components/starship/card/starshipCard.js
@@ -17,7 +17,6 @@ class StarshipCard extends Component {
           <p className='title'> {starship.name} </p>
           <p className='stops'> {Math.floor(starship.stops)} stops </p>
           <span className='property'> {starship.model} </span>
-          <span className='property'> {starship.length} </span>
           <span className='property'> {starship.MGLT} MGLT/h </span>
           <span className='property'> Support {starship.cargo_capacity} Kgs </span>
         </div>
