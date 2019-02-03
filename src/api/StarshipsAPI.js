0f7d69d582d49e00807c315e54f055a1ff037768
Maria Luisa <marialuisaufmg@gmail.com>
2019-02-03 04:36:50 -0200

    change production url API stops (heroku)

--- src/api/StarshipsAPI.js
+++ src/api/StarshipsAPI.js
@@ -1,10 +1,8 @@
 import { Component } from 'react';
 
-// `http://starwars-starships.herokuapp.com/stops/${mglt}`
-
 class StarshipsAPI extends Component {
   getStops = async (mglt) => (
-    await fetch(`http://localhost:8888/stops/${mglt}`)
+    await fetch(`http://starwars-starships.herokuapp.com/stops/${mglt}`)
   );
 }
 
